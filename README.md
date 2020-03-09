5ca7340b77a1e28caa4a360f9d1c326156d8da24
peng.yu <peng.yu@yibuyiyin.com>
2020-03-09 16:01:18 +0800

    add README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# spring-eureka-feign-demo
+了解spring cloud开发模式demo
+
+### gateway - server(eureka) - api - server - client 图解
+[框架图解](pics/Spring开发框架讲解.png)
--- client/src/main/java/cn/feiyu/client/IHello.java
+++ client/src/main/java/cn/feiyu/client/IHello.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "client", path = "/")
+@FeignClient(value = "api", path = "/")
 @RequestMapping(path = {"/"})
 public interface IHello {
     @GetMapping("/hello")
